57c53f536fb6ba264dc4f0945de001dbdfd5fb92
Yin <329301573@qq.com>
2019-05-09 15:52:27 +0800

    基础框架v.01

---
+++ eladmin/src/main.js
@@ -0,0 +1,32 @@
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import animate from 'animate.css';
+import Vuex from 'vuex';
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
+import echarts from 'echarts';
+
+Vue.prototype.$echarts = echarts
+Vue.use(iView);
+Vue.use(Vuex);
+Vue.use(animate);
+Vue.config.productionTip = false;
+
+const store = new Vuex.Store({
+  state:{
+    boxSmall:false,
+  }
+});
+
+
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+});
